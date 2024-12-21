2d10173bc02ea37947d3f2148467e94a7ffa05b9
SHREYAS VERMA <94172366+githubShreyas01@users.noreply.github.com>
2024-12-21 12:21:19 +0530

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
 
+require('dotenv').config();
+
+const usersRoute = require("./routes/usersRoute");
+const connectDb = require("./config/dbConfig");
+connectDb();
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
